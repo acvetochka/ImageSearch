b518069b7e93fa662f06a42d3f01499cdfbd56c0
Alona Kuznietsova <acvetochka@gmail.com>
2024-04-12 00:34:09 +0200

    test webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,32 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
-  devServer: {
-    static: path.join(__dirname, 'src'), // Замініть 'src' на шлях до вашої папки з HTML-файлом
-    port: 8080, // Опціонально, можна змінити порт
-    open: true // Опціонально, відкривати сторінку у браузері автоматично
-  },
+    devServer: {
+      static: path.join(__dirname, 'src'), // Замініть 'src' на шлях до вашої папки з HTML-файлом
+      port: 8080, // Опціонально, можна змінити порт
+      open: true // Опціонально, відкривати сторінку у браузері автоматично
+    },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+        include: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        include: path.resolve(__dirname, 'src/css'),
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        include: path.resolve(__dirname, 'src/img'),
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]', // Вказуємо, щоб зображення зберігалися у папці images
+        },
       },
     ],
   },
